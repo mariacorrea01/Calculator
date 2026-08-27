68c6248289a8c4cfbaf86ce7210a54d4d4cd2cb0
Maria Correa <maria.correa01@globant.com>
2026-08-27 11:32:28 -0500

    Simular suma

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Calculator {
     public double sum(double a, double b) {
-        return a - b;
+        return a + b;
     }
 
     public double subtract(double a, double b) {
